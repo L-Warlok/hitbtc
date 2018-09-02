0457fffc6a7096c122e800a3e28d3ffd8aa2c830
Mike Ellertson <mdellertson@gmail.com>
2018-09-02 16:18:21 -0600

    updated setup.py with version 1.0.5, before publishing to pypi.org.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 VERSION = '1.0.5'
 
-setup(name='hitbtc-wss',
+setup(name='hitbtc_wss',
       version=VERSION,
       description='HitBTC Websocket API Client',
       author='Mike Ellertson',
       author_email='mdellertson@gmail.com',
-      packages=['hitbtc-wss'],
+      packages=['hitbtc_wss'],
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       install_requires=['websocket-client'],
       package_data={'': ['*.md', '*.rst']})
