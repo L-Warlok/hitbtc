c2fb028318221bb0d58a265420ef586207cc648c
Luke Warlik <lukewarlik@kolabnow.com>
2018-10-07 20:24:11 +0800

    Clean unused files

--- hitbtc_wss/__init__.py
+++
@@ -1,4 +0,0 @@
-"""HitBTC WSS API V2.0 Client."""
-
-from hitbtc_wss.client import HitBTC
-
--- main.py
+++
@@ -1,9 +0,0 @@
-
-subs = {'sub_ticker':{'params':{'currencies':['test'],'symbols':['ethdai', 'ethbtc']}}, 'sub_trades':{'params':{'currencies':['this', 'that']}}}
-
-for i in subs:
-      print(getattr(i))
-      for j in subs[i]['params']:
-            for k in subs[i]['params'][j]:
-                  print(j)
-                  print(k)
